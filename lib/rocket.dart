d1dc854f7f1e8ab9b35eea17013d6239619ebe52
William Husum <husum44@gmail.com>
2021-11-29 20:02:29 +0100

    forberedelse til rocketdetails

--- lib/rocket.dart
+++ lib/rocket.dart
@@ -1,32 +1,29 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
 class Rocket {
   final String name;
   final String rocketImageUrl;
   final String country;
+  // final String costPerLaunch;
+  // final String description;
+  // final String mass;
 
   Rocket({
     required this.name,
     required this.rocketImageUrl,
     required this.country,
+    // required this.costPerLaunch,
+    // required this.description,
+    // required this.mass,
   });
 
   factory Rocket.fromJson(Map<String, dynamic> json) {
     return Rocket(
       name: json['name'],
+      // name: json['name'] != null ? ['name'] : "null"
       rocketImageUrl: json['flickr_images'][0],
       country: json['country'],
+      // costPerLaunch: json['cost_per_launch'],
+      // description: json['description'],
+      // mass: json['mass']['kg'],
     );
-  }
-
-  Future<String> fetchImageURL() async {
-    final response = await http.get(Uri.parse(rocketImageUrl));
-    if (response.statusCode == 200) {
-      var jsonData = json.decode(response.body);
-      return jsonData['sprites']['other']['official-artwork']['front_default'];
-    } else {
-      return '';
-    }
   }
 }
