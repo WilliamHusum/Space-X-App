d5de55b9f707e50d07c330e8ed18d8f824eb48c1
William Husum <husum44@gmail.com>
2021-12-03 13:43:37 +0100

    Tilføjet cards

--- lib/launch_detail.dart
+++ lib/launch_detail.dart
@@ -25,92 +25,115 @@ class _LaunchDetailState extends State<LaunchDetail> {
       ),
       body: ListView(
         children: [
+          Card(
+            child: Column(
+              children: <Widget>[
+                Hero(
+                  child: Image.network(
+                      widget.launch.imageUrl ??
+                          "https://cdn1.iconfinder.com/data/icons/ios-11-glyphs/30/rocket-512.png",
+                      height: 200,
+                      scale: 2),
+                  tag: widget.launch.name,
+                ),
+                Text(
+                  widget.launch.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 35),
+                ),
+                Text(
+                  DateFormat('dd-MM-yyyy - kk:mm').format(widget.launch.date),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 12, color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
           //Launch billede
-          Hero(
-            child: Image.network(
-                widget.launch.imageUrl ??
-                    "https://cdn1.iconfinder.com/data/icons/ios-11-glyphs/30/rocket-512.png",
-                height: 200,
-                scale: 2),
-            tag: widget.launch.name,
-          ),
-          Text(
-            widget.launch.name,
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
-          ),
-          Text(
-            DateFormat('dd-MM-yyyy - kk:mm').format(widget.launch.date),
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 12, color: Colors.grey),
-          ),
-          const Divider(color: Colors.black),
-          const Text("Launch details: ",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
-          Text(
-            widget.launch.details ?? "No details",
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 12),
-          ),
-          const Divider(color: Colors.black),
-
-          Container(
-              child:
-                  const Icon(MdiIcons.rocket, size: 50, color: Colors.blueGrey),
-              alignment: Alignment.center),
-
-          const Text("Rocket details: ",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
-          Text(
-            "Name: ${widget.rocket.name ?? "No rocket"}",
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 12),
-          ),
-          Text(
-            "Country: ${widget.rocket.country}",
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 12),
-          ),
-          Text(
-            "Weight: " + widget.rocket.mass + " kg.",
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 12),
-          ),
-          Text(
-            "Price: \$" + widget.rocket.costPerLaunch,
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 12),
-          ),
-          Text(
-            "Number of engines: ${widget.rocket.engines ?? "Info not available"}",
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 12),
-          ),
-          Text(
-            "Fuel amount: ${widget.rocket.fuelAmount ?? "Info not available"} tons",
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 12),
+          Card(
+            child: Column(
+              children: <Widget>[
+                const Text("Launch details: ",
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+                Text(
+                  widget.launch.details ?? "No details",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 12),
+                ),
+              ],
+            ),
           ),
 
-          const Divider(color: Colors.black),
-
-          const Text("Rocket images: ",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
-          Hero(
-            child: Image.network(widget.rocket.rocketImageUrl1,
-                height: 200, scale: 2),
-            tag: widget.rocket.name ?? "No rocket",
+          Card(
+            child: Column(
+              children: <Widget>[
+                Container(
+                    child: const Icon(MdiIcons.rocket,
+                        size: 50, color: Colors.blueGrey),
+                    alignment: Alignment.center),
+                const Text("Rocket details: ",
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+                Text(
+                  "Name: ${widget.rocket.name ?? "No rocket"}",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 12),
+                ),
+                Text(
+                  "Country: ${widget.rocket.country}",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 12),
+                ),
+                Text(
+                  "Weight: " + widget.rocket.mass + " kg.",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 12),
+                ),
+                Text(
+                  "Price: \$" + widget.rocket.costPerLaunch,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 12),
+                ),
+                Text(
+                  "Number of engines: ${widget.rocket.engines ?? "Info not available"}",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 12),
+                ),
+                Text(
+                  "Fuel amount: ${widget.rocket.fuelAmount ?? "Info not available"} tons",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 12),
+                ),
+              ],
+            ),
           ),
-          Hero(
-            child: Image.network(
-                widget.rocket.rocketImageUrl2 ??
-                    "https://cdn1.iconfinder.com/data/icons/ios-11-glyphs/30/rocket-512.png",
-                height: 200,
-                scale: 2),
-            tag: widget.rocket.rocketImageUrl2 ?? "No rocket",
+
+          Card(
+            child: Column(
+              children: <Widget>[
+                const Text("Rocket images: ",
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+                Hero(
+                  child: Image.network(widget.rocket.rocketImageUrl1,
+                      height: 200, scale: 2),
+                  tag: widget.rocket.name ?? "No rocket",
+                ),
+                Hero(
+                  child: Image.network(
+                      widget.rocket.rocketImageUrl2 ??
+                          "https://cdn1.iconfinder.com/data/icons/ios-11-glyphs/30/rocket-512.png",
+                      height: 200,
+                      scale: 2),
+                  tag: widget.rocket.rocketImageUrl2 ?? "No rocket",
+                ),
+              ],
+            ),
           ),
         ],
       ),
--- lib/rocket_detail.dart
+++ lib/rocket_detail.dart
@@ -20,69 +20,94 @@ class _RocketDetailState extends State<RocketDetail> {
       ),
       body: ListView(
         children: [
-          //Sidescroll af billeder
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                SizedBox(
-                    height: 200,
-                    width: 250,
-                    child: Hero(
-                      child: Image.network(widget.rocket.rocketImageUrl1,
-                          scale: 2, height: 250, width: 350),
-                      tag: widget.rocket.name ?? "No rocket",
+          Column(
+            children: <Widget>[
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Card(
+                        child: SizedBox(
+                            height: 200,
+                            width: 250,
+                            child: Hero(
+                              child: Image.network(
+                                  widget.rocket.rocketImageUrl1,
+                                  scale: 2,
+                                  height: 250,
+                                  width: 350),
+                              tag: widget.rocket.name ?? "No rocket",
+                            ))),
+                    Card(
+                        child: SizedBox(
+                      height: 200,
+                      width: 250,
+                      child: Hero(
+                        child: Image.network(
+                            widget.rocket.rocketImageUrl2 ?? "",
+                            scale: 2),
+                        tag: widget.rocket.name! + "2",
+                      ),
                     )),
-                SizedBox(
-                  height: 200,
-                  width: 250,
-                  child: Hero(
-                    child: Image.network(widget.rocket.rocketImageUrl2 ?? "",
-                        scale: 2),
-                    tag: widget.rocket.name! + "2",
-                  ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Card(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  widget.rocket.name ?? "No rocket",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 35),
+                ),
+                const Text("Description: ",
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+                Text(
+                  widget.rocket.description,
+                  textAlign: TextAlign.center,
                 ),
               ],
             ),
           ),
-          Text(
-            widget.rocket.name ?? "No rocket",
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 35),
+          Card(
+            child: Column(
+              children: <Widget>[
+                const Text("Technical details: ",
+                    textAlign: TextAlign.center,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
+                Text(
+                  "Weight: " + widget.rocket.mass + " kg.",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w300, fontSize: 12),
+                ),
+                Text(
+                  "Price: \$" + widget.rocket.costPerLaunch,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w300, fontSize: 12),
+                ),
+                Text(
+                  "Number of engines: ${widget.rocket.engines ?? "Info not available"}",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w300, fontSize: 12),
+                ),
+                Text(
+                  "Fuel amount: ${widget.rocket.fuelAmount ?? "Info not available"} tons",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w300, fontSize: 12),
+                ),
+              ],
+            ),
           ),
-          const Divider(color: Colors.black),
-          const Text("Description: ",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
-          Text(
-            widget.rocket.description,
-            textAlign: TextAlign.center,
-          ),
-          const Divider(color: Colors.black),
-          const Text("Technical details: ",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14)),
-          Text(
-            "Weight: " + widget.rocket.mass + " kg.",
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 12),
-          ),
-          Text(
-            "Price: \$" + widget.rocket.costPerLaunch,
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 12),
-          ),
-          Text(
-            "Number of engines: ${widget.rocket.engines ?? "Info not available"}",
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 12),
-          ),
-          Text(
-            "Fuel amount: ${widget.rocket.fuelAmount ?? "Info not available"} tons",
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontWeight: FontWeight.w300, fontSize: 12),
-          ),
-          const Divider(color: Colors.black),
         ],
       ),
     );
